5c9272818e3b4f0273c787669adb77179a910ce8
CitricCanine <147597645+CitricCanine@users.noreply.github.com>
2024-10-08 12:15:33 +0100

    V_04
    added lever key and locks
    levers can make bridges
    keys work on locks
    locks destroy door objects

---
+++ First_Person_Experience/Assets/Scripts/PlayerInteractions.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractions : MonoBehaviour
+{
+    public Collider triggerColl;
+    GameManager gmSc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gmSc = GameObject.Find("GameManager").GetComponent<GameManager>();
+        gmSc.infoText.text = " ";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggerColl == null)
+        {
+            gmSc.infoText.text = " ";
+        }
+        if (triggerColl != null && Input.GetKeyDown(KeyCode.E))
+        {
+            if(triggerColl.gameObject.CompareTag("Lock") && gmSc.hasKey)
+            {
+                gmSc.hasKey = false;
+                gmSc.infoText.text = " ";
+                Destroy(triggerColl.gameObject);
+            }
+            if (triggerColl.gameObject.CompareTag("Lever"))
+            {
+                Lever leverSC = triggerColl.gameObject.GetComponent<Lever>();
+                leverSC.isOn = !leverSC.isOn;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        triggerColl = other;
+        if (other.gameObject.CompareTag("Key"))
+        {
+            gmSc.hasKey = true;
+            Destroy(other.gameObject);
+        }
+    
+        if (other.gameObject.CompareTag("Lock"))
+        {
+            if (gmSc.hasKey)
+            {
+                gmSc.infoText.text = "Press E To Interact";
+            }
+            else
+            {
+                gmSc.infoText.text = "You Need A Key To Open This";
+            }
+        }
+    
+        if (other.gameObject.CompareTag("Lever"))
+        {
+            gmSc.infoText.text = "Press E To Switch";
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        triggerColl = null;
+    }
+}
