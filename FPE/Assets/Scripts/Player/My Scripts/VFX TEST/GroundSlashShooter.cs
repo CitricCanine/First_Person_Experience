c96cd0b4ca1c1661093ff29bda623b10ef23643e
CitricCanine <147597645+CitricCanine@users.noreply.github.com>
2024-12-16 15:39:50 +0000

    V_12
    started doing magic attacks
    ground slash has bnen started, basics work
    now its time to make it look nicer and keep adding attacks

---
+++ FPE/Assets/Scripts/Player/My Scripts/VFX TEST/GroundSlashShooter.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class GroundSlashShooter : MonoBehaviour
+{
+    public Transform sp;
+
+    public Camera cam;
+    public GameObject projectile;
+    public Transform firePoint;
+    public float fireRate = 4;
+
+    private Vector3 destination;
+    private float timeToFire;
+    private GroundSlash groundSlashScript;
+    void Update()
+    {
+        if(Input.GetButton("Fire1") && Time.time >= timeToFire)
+        {
+            timeToFire = Time.time + 1/fireRate;
+            ShootProjectile();
+        }        
+    }
+
+    void ShootProjectile()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+        destination = ray.GetPoint(1000);
+
+        InstantiateProjectile();
+    }
+
+    void InstantiateProjectile()
+    {
+        // var projectileObj = Instantiate (projectile, firePoint.position, firePoint.rotation);
+        var projectileObj = Instantiate (projectile, sp.position, sp.rotation);
+
+        groundSlashScript = projectileObj.GetComponent<GroundSlash>();
+        RotateToDestination(projectileObj, destination, true);
+        projectileObj.GetComponent<Rigidbody>().velocity = transform.forward * groundSlashScript.speed;
+    }
+
+    void RotateToDestination(GameObject obj, Vector3 destination, bool onlyY)
+    {
+        var direction = destination - obj.transform.position;
+        var rotation = Quaternion.LookRotation(direction);
+
+        if(onlyY)
+        {
+            rotation.x = 0;
+            rotation.z = 0;
+        }
+
+        obj.transform.localRotation = Quaternion.Lerp(obj.transform.rotation, rotation, 1);
+    }
+}
